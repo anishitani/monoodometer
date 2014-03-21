d42e248a1ea99209aa97b878910ff0781e8dc450
anishitani <anishitani@98588553-9cc4-4573-a2ad-157070dfa0ef>
2014-03-21 02:16:54 +0000

    Mod: Adding libviso2 via ExternalProject cmake module Obs: It is possible to add via add_subdirectory()

--- mgbo/trunk/CMakeModules/FindVISO2.cmake
+++ mgbo/trunk/CMakeModules/FindVISO2.cmake
@@ -4,6 +4,8 @@
 #  VISO2_INCLUDE_DIR - the libVISO2 include directories
 #  VISO2_LIBRARY - link these to use libVISO2
 
+message(STATUS "Include path = ${CMAKE_INCLUDE_PATH}")
+
 FIND_PATH(
   VISO2_INCLUDE_DIR
   NAMES viso_mono.h
@@ -18,6 +20,7 @@ FIND_LIBRARY(
   VISO2_LIBS
   NAMES viso2
   PATHS
+    ${CMAKE_LIBRARY_PATH}
     ${CMAKE_SOURCE_DIR}/../libviso2/lib
     ${CMAKE_SOURCE_DIR}/../viso2/lib
     /usr/lib

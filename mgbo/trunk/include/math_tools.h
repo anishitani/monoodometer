373ec9d49148e7259466ad37da34c8187ad7bd26
anishitani <anishitani@98588553-9cc4-4573-a2ad-157070dfa0ef>
2014-03-22 22:43:58 +0000

    Mod: CMake is now able to perform the project installation. Still no installation rule, but Jenkins is now able to complete the run. Mod: The MathTools lib was separated into two files (Header and Source)

--- mgbo/trunk/include/math_tools.h
+++ mgbo/trunk/include/math_tools.h
@@ -9,83 +9,15 @@
 #define MATH_TOOLS_H_
 
 #include <climits>
-
 #include <opencv2/opencv.hpp>
-
 #include <expm.hpp>
 
-inline void gradient(cv::Mat I, cv::Mat &Ix, cv::Mat &Iy)
-{
-	cv::Matx13f dx(-0.5, 0, 0.5);
-	cv::Matx31f dy(-0.5, 0, 0.5);
-
-	cv::filter2D(I, Ix, CV_32F, dx);
-	cv::filter2D(I, Iy, CV_32F, dy);
-
-	cv::subtract(I.col(1), I.col(0), Ix.col(0));
-	cv::subtract(I.col(I.cols - 1), I.col(I.cols - 2), Ix.col(I.cols - 1));
-
-	cv::subtract(I.row(1), I.row(0), Iy.row(0));
-	cv::subtract(I.row(I.rows - 1), I.row(I.rows - 2), Iy.row(I.rows - 1));
-}
-
-inline void meshgrid(int x0, int y0, int width, int height, cv::Mat &gridx,
-		cv::Mat &gridy)
-{
-	cv::Mat rowx(1, width, CV_32F), coly(height, 1, CV_32F);
-	for (int i = 0; i < width; i++)
-		rowx.at<float>(0, i) = x0 + i;
-	for (int i = 0; i < height; i++)
-		coly.at<float>(i, 0) = y0 + i;
-
-	cv::repeat(rowx, height, 1, gridx);
-	cv::repeat(coly, 1, width, gridy);
-}
-
-inline cv::Mat skew(cv::Mat a)
-{
-	return (cv::Mat_<float>(3, 3) << 0, -a.at<float>(2), a.at<float>(1), a.at<
-			float>(2), 0, -a.at<float>(0), -a.at<float>(1), a.at<float>(0), 0);
-}
-
-inline cv::Mat expm(cv::Mat x)
-{
-	assert(x.rows == x.cols);
-
-	boost::numeric::ublas::matrix<float> h(x.rows, x.cols);
-	for (int i = 0; i < x.rows; i++)
-	{
-		for (int j = 0; j < x.cols; j++)
-		{
-			h(i, j) = x.at<float>(i, j);
-		}
-	}
-
-	h = expm_pad(h);
-
-	cv::Mat A(x.rows, x.cols, CV_32F);
-	for (int i = 0; i < x.rows; i++)
-	{
-		for (int j = 0; j < x.cols; j++)
-		{
-			A.at<float>(i, j) = h(i, j);
-		}
-	}
-
-	return A;
-}
-
-inline bool IsNumber(double x)
-{
-	// This looks like it should always be true,
-	// but it's false if x is a NaN.
-	return (x == x);
-}
-
-inline bool IsFiniteNumber(double x)
-{
-
-	return (x <= DBL_MAX && x >= -DBL_MAX);
-}
+void gradient(cv::Mat I, cv::Mat &Ix, cv::Mat &Iy);
+void meshgrid(int x0, int y0, int width, int height, cv::Mat &gridx,
+		cv::Mat &gridy);
+cv::Mat skew(cv::Mat a);
+cv::Mat expm(cv::Mat x);
+bool IsNumber(double x);
+bool IsFiniteNumber(double x);
 
 #endif /* MATH_TOOLS_H_ */
